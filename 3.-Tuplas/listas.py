a0d19af9b1ab6e8b9c58b3ce9a322af9be743a9d
Adrian <reacktive434@gmail.com>
2022-06-02 23:02:22 -0500

    Personalizacion de las carpetas 2, 3 y 4

--- 3.-Tuplas/listas.py
+++ 3.-Tuplas/listas.py
@@ -1,13 +1,13 @@
-cursos = ['Python', 'Django', 'Flask']
+cursos = ['Python', 'Django', 'Flask']  # Esta es una lista
 
-cursos_tupla = tuple(cursos)
+cursos_tupla = tuple(cursos)  # Convertimos la lista cursos en una tupla
 
 print(cursos_tupla)
 print(type(cursos_tupla))
 
-niveles = ('Básico', 'Intermedio', 'Avanzado')
+niveles = ('Básico', 'Intermedio', 'Avanzado')  # Esta es una tupla
 
-niveles_lista = list(niveles)
+niveles_lista = list(niveles)  # Convertimos la tupla niveles en una lista
 
 print(niveles_lista)
 print(type(niveles_lista))
--- 4.-Strings/alineacion.py
+++ 4.-Strings/alineacion.py
@@ -1,9 +1,15 @@
 mensaje = 'Hola mundo!'
+izquierda = 'ADRIAN'  #'Este es un mensaje justificado a la izquierda'
+derecha = 'MEDINA'  #'Este es un mensaje justificado a la derecha'
 
 # ljust -> Justificar a la Izquierda
 # rjust -> Justificar a la Derecha
 # center -> Centrar
 
 mensaje = mensaje.center(20)
+izquierda = izquierda.ljust(20)
+derecha = derecha.rjust(20)
 
 print(mensaje)
+print(izquierda)
+print(derecha)
